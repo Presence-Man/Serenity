98ebe1a36bb5aa34209f8ba03a06d149bab08e0b
xxAROX <arox.email.0@gmail.com>
2024-07-23 17:38:17 +0200

    More debug

--- src/xxAROX/Presence-Man/SerenityJS/PresenceMan.ts
+++ src/xxAROX/Presence-Man/SerenityJS/PresenceMan.ts
@@ -71,7 +71,8 @@ export default class PresenceMan {
         console.log(this.getConfig());
         
         if (this.getConfig().default_presence.enabled) {
-            this.serenity.on("PlayerJoined", (event) => {
+            this.serenity.on("PlayerJoined", async (event) => {
+                this.logger.info(await WebUtils.isFromSameHost(event.player.session.connection.rinfo.address));
                 this.logger.info("Player " + event.player.username + " joined!");
                 this.setActivity(event.player, DefaultActivities.activity());
             })
